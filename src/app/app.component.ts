9248f061cedbac6cb0cef3f3de9c86ccd1a5a9ac
bpiasecki <bartek0894@gmail.com>
2021-04-19 08:29:42 +0200

    layout fixes and custom scroll addition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { MatDrawer } from '@angular/material/sidenav';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { map, shareReplay, tap } from 'rxjs/operators';
+import { map, shareReplay } from 'rxjs/operators';
 import { AuthService } from './auth/auth.service';
-import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
 
 @Component({
   selector: 'app-root',
@@ -30,9 +28,7 @@ export class AppComponent {
       shareReplay()
     );
 
-  itemsRef: AngularFireList<any>;
-  items: Observable<any[]>;
-  constructor(private breakpointObserver: BreakpointObserver, private authService: AuthService, private router: Router, private db: AngularFireDatabase) {
+  constructor(private breakpointObserver: BreakpointObserver, private authService: AuthService, private router: Router) {
     this.authService.authState$.subscribe((user) => {
       console.log(user)
       if (user) {
@@ -44,38 +40,12 @@ export class AppComponent {
       }
       this.dataLoaded = true;
     })
-    this.itemsRef = db.list<any>('items');
-    this.items = db.list<any>('items').snapshotChanges().pipe(
-      map(changes =>
-        changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
-      )
-    );
   }
 
   logout() {
     this.authService.logout().then(() => {
       this.router.navigate(['login'])
     });
-  }
-
-  addData() {
-    const data = { name: "AudunCoffee", rating: 8 }
-    // const obj = this.db.object('coffee');
-    // this.itemData = obj.valueChanges().pipe(tap((res) => console.log(res)));
-    // obj.set(data).then(res => { console.log(res) }, err => Promise.reject(err));
-
-
-    const itemsRef = this.db.list('items');
-    itemsRef.push(data);
-  }
-
-  itemData: Observable<unknown>;
-  getData() {
-    // this.db.list('coffees').set().subscribe((result) => console.log(result));
-  }
-
-  updateItem(key: string, newValue: string) {
-    this.itemsRef.update(key, {name: newValue})
   }
 
 }

204305517206f3b201c9d4376b78536086c779e8
bpiasecki <bartek0894@gmail.com>
2021-06-09 09:10:50 +0200

    category name pipe added

--- src/app/shared/pipes/categoryName.pipe.ts
+++ src/app/shared/pipes/categoryName.pipe.ts
@@ -1,8 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { CategoryItem } from '@core/models/categories/categoryItem.model';
+import { ItemKeyWithData } from '@core/models/itemKeyWithData.model';
 
-@Pipe({name: 'categoryName'})
-export class NamePipe implements PipeTransform {
-    transform(value: any): any {
-        
+@Pipe({ name: 'categoryName' })
+export class CategoryNamePipe implements PipeTransform {
+
+    transform(categoryKey: string, categories: ItemKeyWithData<CategoryItem>[]): string {
+        const category = categories.find(item => item.key === categoryKey)?.data;
+        const categoryParent = categories.find(item => item.key === category?.parent)?.data;
+        return `${categoryParent ? categoryParent.name + ' - ' : ''}${category?.name ?? "Brak kategorii"}`;
     }
 }
